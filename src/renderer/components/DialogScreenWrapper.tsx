5334fc78c6ba225fc79338674c6f5abe07b9d3dc
lorem ipsum <lorem.ipsum.crypto@gmail.com>
2024-02-15 22:13:39 -0500

    linting

--- src/renderer/components/DialogScreenWrapper.tsx
+++ src/renderer/components/DialogScreenWrapper.tsx
@@ -1,8 +1,8 @@
 // this is a wrapper component that will cover the entire screen when a dialog is open
 // and close when anything outside of the dialog is clicked
 export default function DialogScreenWrapper(props: {
-  isDialogOpen: boolean,
-  setIsDialogOpen: (isOpen: boolean) => void,
+  isDialogOpen: boolean;
+  setIsDialogOpen: (isOpen: boolean) => void;
 }) {
   if (!props.isDialogOpen) return null;
   return (
@@ -10,5 +10,5 @@ export default function DialogScreenWrapper(props: {
       className="z-2 absolute left-0 top-0 w-screen h-screen"
       onClick={() => props.setIsDialogOpen(false)}
     />
-  )
+  );
 }
